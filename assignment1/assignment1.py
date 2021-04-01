0fe865750728dd386f092a157ce829fa9b4fd8d5
Karim-1 <54839428+Karim-1@users.noreply.github.com>
2021-04-01 10:56:21 +0200

    Update assignment1.py

--- assignment1/assignment1.py
+++ assignment1/assignment1.py
@@ -1,6 +1,11 @@
-def karim():
+import pandas as pd
+
+def karim(data):
     pass
 
-def daan():
+def daan(data):
     pass
 
+ODI_2021 = pd.read_csv("ODI-2021.csv", header=None, skiprows=1)
+karim(ODI_2021)
+daan(ODI_2021)
