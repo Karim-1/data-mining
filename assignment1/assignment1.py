2028fcfc9ed3d9e607a60254a59cd751e5bfaa31
Daan Moll <daanmoll@hotmail.com>
2021-04-01 10:56:19 +0200

    merge karim

--- assignment1/assignment1.py
+++ assignment1/assignment1.py
@@ -1,6 +1,20 @@
+import csv
+from os import name
+from tqdm import tqdm
+import numba
+
 def karim():
     pass
 
 def daan():
-    pass
+    with open("ODI-2021.csv") as csvfile:
+        data = csv.reader(csvfile)
+        for row in data:
+            print(row)
+            break
+
+
+if __name__ == "__main__":
+    print("hoi")
+    daan()
 
