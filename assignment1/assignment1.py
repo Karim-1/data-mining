ca6822bb48463ad5f3e8af92d0a05da9f095a060
Karim-1 <54839428+Karim-1@users.noreply.github.com>
2021-04-07 11:12:12 +0200

    updates

--- assignment1/assignment1.py
+++ assignment1/assignment1.py
@@ -1,11 +1,25 @@
-import pandas as pd
+import csv
 
 def karim(data):
-    
+    for line in data:
+        # tranform programme names
+        programme = line[1].upper()
+        # remove words that are not needed
+        stopwords = ['MSC', 'MASTERS', 'MASTER', 'OF', 'SCIENCE', 'IN']
+        words = ' '.join(filter(lambda x: x.lower() not in stopwords, programme.split()))
+        letters = [word[0].upper() for word in row[1].split()]
+        print("".join(letters))
 
 def daan(data):
     pass
 
-ODI_2021 = pd.read_csv("ODI-2021.csv", header=None, skiprows=1)
+# ODI_2021 = pd.read_csv("ODI-2021.csv", header=None, skiprows=1)
+
+ODI_2021 = []
+with open("ODI-2021.csv") as file:
+    reader = csv.reader(file)
+    for row in reader:
+        ODI_2021.append(row)
+
 karim(ODI_2021)
-daan(ODI_2021)
+# daan(ODI_2021)
