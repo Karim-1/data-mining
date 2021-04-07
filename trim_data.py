98afb66f409ed51a25b4547522ba7f7ec0fc9f77
Daan Moll <daanmoll@hotmail.com>
2021-04-07 12:05:13 +0200

    pandas

--- trim_data.py
+++ trim_data.py
@@ -1,26 +1,41 @@
 import csv
+import datetime
+import pandas as pd
+
 
 def trim_programma():
+    programme = df.iloc[:, 1]
     pass
 
-def trim_prev_courses(arg1, arg2, arg3, arg4):
+def trim_prev_courses():
     # marlon
+    courses = df.iloc[:, 2:6]
     pass
-
-def trim_gender(arg1):
-    # daan
-    pass
-
-def trim_choc(arg1):
-    pass 
-
 
 def trim_gender():
+    # daan
+    gender = df.iloc[:, 6]
+    # all genders are conform the list, so no trimming needed
+    genders = ["male", "female", "unknown"]
+    if gender not in genders:
+        print(gender)
+    pass
+
+def trim_choc():
+    chocolate = df.iloc[:, 7]
+    # five radio options from the form so no trimming needed
+    pass
+
+def trim_birthday():
+    months = ['january', 'february', 'march', 'april', 'may', 'june', 'july',
+          'august', 'september', 'october', 'november', 'december']
     pass
 
 
-ODI_2021 = []
-with open("ODI-2021.csv") as file:
-    reader = csv.reader(file)
-    for row in reader:
-        ODI_2021.append(row)
+df = pd.read_csv("ODI-2021.csv")
+
+trim_programma()
+trim_prev_courses()
+trim_gender()
+trim_choc()
+trim_birthday()
