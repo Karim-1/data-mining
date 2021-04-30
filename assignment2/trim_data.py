ea07526470d978732d244bd26027cbde5cd7ee80
Karim-1 <54839428+Karim-1@users.noreply.github.com>
2021-04-30 10:52:17 +0200

    remove columns with missing values

--- assignment2/trim_data.py
+++ assignment2/trim_data.py
@@ -1,3 +1,11 @@
+'''
+trims the data, add
+_________________________________________________________________
+try-excepts have been added in each function to check whether the 
+column has not been removed due to >95% of data values missing.
+'''
+
+import copy
 import dateparser
 import matplotlib.pyplot as plt
 import numpy as np
@@ -6,17 +14,33 @@ import pandas as pd
 from numpy.core.numeric import NaN
 
 
-
-
-
 df = pd.read_csv("small_data/3000_training_set_VU_DM.csv")
 
+def trim_cols(data):
+    '''
+    removes columns with >95% missing data values
+    '''
+    df = data.copy()
+    total_values = len(df.index)
+    
+    for col in df:
+        missing_values = sum(df[col].isnull()==True)
+        if missing_values > .95 * total_values:
+            print('column', col, 'removed')
+            df.drop(col, 1)
+
+    return df
 
 def trim_dates(df):
     '''
     replaces datetime column with separate month and daypart column
     '''
-    dates = df['date_time']
+    try:
+        dates = df['date_time']
+    except:
+        print("column 'date_time' has been removed, can't be trimmed")
+        return df
+    
 
     months, dayparts = [], []
     for i in range(len(dates)):
@@ -48,13 +72,9 @@ def trim_dates(df):
     df['month'] = months
     df['daypart'] = dayparts
 
-    
-    cols = df.columns.tolist()
-    print(cols)
     # change column order df
+    cols = df.columns.tolist()
     cols = cols[1:2] + cols[-2:] + cols[4:-3]
-    print(cols)
-
     df = df[cols]
 
     return df
@@ -63,14 +83,20 @@ def trim_avg_rating(df):
     '''
     rounds average rating to halves
     '''
-    ratings = df['visitor_hist_starrating']
+    try:
+        ratings = df['visitor_hist_starrating']
+    except:
+        print("column 'visitor_hist_starrating' has been removed, can't be trimmed")
+        return df
+    
     
     # round ratings to halves, or add NA if data is missing
     for i in range(len(ratings)):
         try:
             ratings[i] = round(ratings[i] * 2) / 2
         except:
-            ratings[i] = 'NA'
+            # ratings[i] = 'NA'
+            continue
         
     return df
 
@@ -80,7 +106,11 @@ def trim_avg_spent(df):
 
     TODO: kijken of de distributie heel hoog is rondom een getal, en daar misschien meer 'bins' maken
     '''
-    spent = df['visitor_hist_adr_usd']
+    try:
+        spent = df['visitor_hist_adr_usd']
+    except:
+        print("column 'visitor_hist_adr_usd' has been removed, can't be trimmed")
+        return df
 
     # round to every 50 dollars spent or add 'NA'
     for i in range(len(spent)):
@@ -88,18 +118,22 @@ def trim_avg_spent(df):
             multiplier = round(spent[i]/50)
             spent[i] = multiplier * 50
         except:
-            spent[i] = 'NA'
+            # spent[i] = 'NA'
+            continue
         
     return df
 
 def trim_data(df):
+    # remove columsn with >95% missing data values
+    trim_cols(df)
+
+    # restructure data
     df = trim_dates(df)
-    # df = trim_avg_rating(df)
-    # df = trim_avg_spent(df)
-    # df = trim_avg_spent(df)
+    df = trim_avg_rating(df)
+    df = trim_avg_spent(df)
+    df = trim_avg_spent(df)
 
     return df
     
 data = trim_data(df)
-print(data)
 
