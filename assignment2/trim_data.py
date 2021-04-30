d79fdfcb06e2b9d37040330a83c1e03d98e026e0
Karim-1 <54839428+Karim-1@users.noreply.github.com>
2021-04-30 15:37:14 +0200

    meer trimzz

--- assignment2/trim_data.py
+++ assignment2/trim_data.py
@@ -31,10 +31,12 @@ def trim_cols(data):
 
     return df
 
-def trim_dates(df):
+def trim_dates(data):
     '''
     replaces datetime column with separate month and daypart column
     '''
+    df = data.copy()
+
     try:
         dates = df['date_time']
     except:
@@ -79,10 +81,12 @@ def trim_dates(df):
 
     return df
 
-def trim_avg_rating(df):
+def trim_avg_rating(data):
     '''
     rounds average rating to halves
     '''
+    df = data.copy()
+
     try:
         ratings = df['visitor_hist_starrating']
     except:
@@ -95,17 +99,18 @@ def trim_avg_rating(df):
         try:
             ratings[i] = round(ratings[i] * 2) / 2
         except:
-            # ratings[i] = 'NA'
-            continue
+            ratings[i] = 'NA'
         
     return df
 
-def trim_avg_spent(df):
+def trim_avg_spent(data):
     '''
     trims average price spent per night for customers
 
     TODO: kijken of de distributie heel hoog is rondom een getal, en daar misschien meer 'bins' maken
     '''
+    df = data.copy()
+
     try:
         spent = df['visitor_hist_adr_usd']
     except:
@@ -118,22 +123,137 @@ def trim_avg_spent(df):
             multiplier = round(spent[i]/50)
             spent[i] = multiplier * 50
         except:
-            # spent[i] = 'NA'
-            continue
+            spent[i] = 'NA'
         
     return df
 
-def trim_data(df):
-    # remove columsn with >95% missing data values
-    trim_cols(df)
+def trim_loc_score(data):
+    '''
+    rounds location score to halves or adds 'NA' for missing values
+    '''
+    df = data.copy()
 
+    try:
+        loc_score1 = df['prop_location_score1']
+        loc_score2 = df['prop_location_score2']
+    except:
+        print("column 'prop_location_score1' or 'prop_location_score2' has been removed, can't be trimmed")
+        return df
+
+    
+    for i in range(len(loc_score1)):
+        try:
+            loc_score1[i] = round(loc_score1[i] * 2) / 2
+        except:
+            loc_score1[i] = 'NA'
+            
+        try:
+            loc_score2[i] = round(loc_score2[i] * 2) / 2
+        except:
+            loc_score2[i] = 'NA'
+
+    return df
+
+def trim_hist_price(data):
+    '''
+    rounds 
+    '''
+    df = data.copy()
+
+    try:
+        hist_price = df['prop_log_historical_price']
+    except:
+        print("column 'prop_log_historical_price' has been removed, can't be trimmed")
+        return df
+    
+    for i in range(len(hist_price)):    
+        if hist_price[i] > 0:
+            hist_price[i] = round(hist_price[i] * 2) / 2
+        else:
+            hist_price[i] = 'NA'
+    
+    return df
+
+def trim_price(data):
+    df = data.copy()
+    try:
+        price = df['price_usd']
+    except:
+        print("column 'price_usd' has been removed, can't be trimmed")
+        return df
+
+    
+    for i in range(len(price)):    
+        price[i] = round(price[i])
+
+    return df
+    
+
+def trim_book_window(data):
+    '''
+    changes booking window from days to weeks
+    '''
+    df = data.copy()
+    try:
+        booking_window = df['srch_booking_window']
+    except:
+        print("column 'srch_booking_window' has been removed, can't be trimmed")
+        return df
+
+    
+    for i in range(len(booking_window)):
+        try:
+            booking_window[i] = round(booking_window[i]/7)
+        except:
+            booking_window[i] = 'NA'
+
+    return df
+
+
+def trim_dest_dist(data):
+    '''
+    divides distance to destination by 100 miles
+    '''
+    df = data.copy()
+
+    try:
+        dest_dist = df['orig_destination_distance']
+    except:
+        print("column 'orig_destination_distance' has been removed, can't be trimmed")
+        return df
+
+    for i in range(len(dest_dist)):
+        try:
+            dest_dist[i] = round(dest_dist[i]/100)
+        except:
+            # booking_window[i] = 'NA'
+            dest_dist[i] = 'NA'
+
+    return df    
+
+
+
+
+def trim_data(df):
+    # remove hotels that are not picked
+    # TODO
+
+    # remove columns with >95% missing data values
+    trim_cols(df)
+    
     # restructure data
     df = trim_dates(df)
     df = trim_avg_rating(df)
     df = trim_avg_spent(df)
-    df = trim_avg_spent(df)
-
+    df = trim_loc_score(df)
+    df = trim_hist_price(df)
+    df = trim_price(df)
+    df = trim_book_window(df)
+    df = trim_dest_dist(df)
+    
     return df
     
 data = trim_data(df)
+
+data.to_csv('small_data/trimmed_data.csv')
 
